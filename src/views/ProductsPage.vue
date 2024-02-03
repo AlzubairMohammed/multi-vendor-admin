b4645ffa3289afba5b0406de6104f423aa533f9a
AlzubairMohammed <dev.alzubair@gmail.com>
2024-02-04 02:28:44 +0300

    Add Table, Products components'

--- src/views/ProductsPage.vue
+++ src/views/ProductsPage.vue
@@ -1,7 +1,9 @@
 <script setup>
 import AppLayout from "@/components/AppLayout.vue";
+import AppNavbar from "@/components/AppNavbar.vue";
+import SearchAddButton from "@/components/Tables/SearchAddButton.vue";
+import ProductTable from "@/components/Products/ProductsTable.vue";
 import { FwbModal } from "flowbite-vue";
-import AppNavbar from "../components/AppNavbar.vue";
 import { onMounted, ref, inject } from "vue";
 import { useStore } from "vuex";
 const checkCookie = inject("checkCookie");
@@ -38,9 +40,8 @@ let data = ref({
 let products = ref([]);
 let categories = ref([]);
 let attributes = ref([]);
-let page = 1;
-let tot = 1;
-let search_term = "";
+
+// let search_term = "";
 let isShowAddModal = ref(false);
 let isShowEditModal = ref(false);
 
@@ -62,7 +63,7 @@ const handleFileChange = (event) => {
 const add = async () => {
   await store.dispatch("addProduct", data.value);
 };
-const delet = async (id) => {
+const deletProduct = async (id) => {
   await store.dispatch("deleteProduct", id);
 };
 onMounted(async () => {
@@ -95,129 +96,12 @@ onMounted(async () => {
     <app-navbar :title="'المنتجات'"></app-navbar>
     <div class="w-full py-8 px-4">
       <!-- <page-header title="المنتجات" /> -->
-      <div class="flex justify-between items-center py-4">
-        <div class="relative">
-          <input
-            type="email"
-            class="peer py-3 pl-6 pr-10 ps-11 block w-full bg-gray-100 border-transparent rounded-lg text-sm focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none dark:bg-gray-700 dark:border-transparent dark:text-gray-400 dark:focus:ring-gray-600"
-            placeholder="بحث"
-            v-model="search_term"
-            @input="search()"
-          />
-          <div
-            class="absolute right-2 inset-y-0 start-0 flex items-center pointer-events-none ps-4 peer-disabled:opacity-50 peer-disabled:pointer-events-none"
-          >
-            <box-icon color="grey" name="search"></box-icon>
-          </div>
-        </div>
-
-        <button
-          @click="showAddModal()"
-          type="button"
-          class="py-3 px-4 inline-flex items-center gap-x-2 text-sm font-semibold rounded-lg border border-transparent bg-gray-800 text-white hover:bg-gray-900 disabled:opacity-50 disabled:pointer-events-none dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600 dark:bg-white dark:text-gray-800"
-        >
-          <box-icon color="white" name="plus"></box-icon>
-        </button>
-      </div>
-
-      <div class="table w-full">
-        <div class="relative overflow-x-auto shadow-md sm:rounded-lg">
-          <table
-            class="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400"
-          >
-            <thead
-              class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400"
-            >
-              <tr>
-                <th scope="col" class="px-6 py-3 text-center">الرقم</th>
-                <th scope="col" class="px-6 py-3 text-center">الاسم</th>
-                <th scope="col" class="px-6 py-3 text-center">القسم</th>
-                <th scope="col" class="px-6 py-3 text-center">سعر المخزن</th>
-                <th scope="col" class="px-6 py-3 text-center">سعر البيع</th>
-                <th scope="col" class="px-6 py-3 text-center">الصورة</th>
-                <th scope="col" class="px-6 py-3 text-center"></th>
-              </tr>
-            </thead>
-            <tbody>
-              <tr
-                v-for="(app, index) in products"
-                :key="app.id"
-                class="odd:bg-white odd:dark:bg-gray-900 even:bg-gray-50 even:dark:bg-gray-800 border-b dark:border-gray-700"
-              >
-                <th
-                  scope="row"
-                  class="px-6 py-4 text-center font-medium text-gray-900 whitespace-nowrap dark:text-white"
-                >
-                  {{ (index += 1) }}
-                </th>
-                <td class="px-6 py-4 text-center">{{ app?.name }}</td>
-                <td class="px-6 py-4 text-center">
-                  {{ app?.sub_category?.name }}
-                </td>
-                <td class="px-6 py-4 text-center">{{ app?.buy_price }}</td>
-                <td class="px-6 py-4 text-center">{{ app?.sale_price }}</td>
-                <td class="px-6 py-4 text-center flex justify-center">
-                  <div
-                    class="flex justify-center"
-                    style="width: 80px; height: 60px; overflow: hidden"
-                  >
-                    <img
-                      style="width: 100%; height: auto"
-                      :src="`http://89.116.236.251:8000/${
-                        app?.image?.split('public/')[1]
-                      }`"
-                      alt=""
-                    />
-                  </div>
-                </td>
-                <td class="px-6 py-4">
-                  <button
-                    @click="showEditModal(app)"
-                    type="button"
-                    class="font-medium mx-4 text-green-600 dark:text-blue-500 hover:underline"
-                  >
-                    عرض
-                  </button>
-                  <button
-                    @click="showEditModal(app)"
-                    type="button"
-                    class="font-medium mx-4 text-blue-600 dark:text-blue-500 hover:underline"
-                  >
-                    تعديل
-                  </button>
-                  <button
-                    @click="delet(app)"
-                    type="button"
-                    class="font-medium text-red-600 mx-2 dark:text-red-500 hover:underline"
-                  >
-                    حذف
-                  </button>
-                </td>
-              </tr>
-            </tbody>
-          </table>
-
-          <nav class="flex justify-center my-4" v-if="tot > 1">
-            <ul class="inline-flex -space-x-px text-base h-10">
-              <li
-                v-for="page_number in tot"
-                @click="handlePagination(page_number)"
-                :key="page_number"
-              >
-                <a
-                  :class="{
-                    'bg-blue-600 text-white hover:text-gray-100 hover:bg-blue-900 ':
-                      page_number == page,
-                  }"
-                  class="flex items-center justify-center px-4 h-10 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
-                  >{{ page_number }}</a
-                >
-              </li>
-            </ul>
-          </nav>
-        </div>
-      </div>
-
+      <SearchAddButton :showModel="showAddModal" />
+      <ProductTable
+        :showEditModal="showEditModal"
+        :deletProduct="deletProduct"
+        :products="products"
+      />
       <fwb-modal v-if="isShowAddModal" @close="closeAddModal">
         <template #header>
           <div class="flex items-center text-lg">اضافة</div>
